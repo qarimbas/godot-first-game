018f09bec7850830008c22617437673ebe9aef7f
qarimbas <71667807+qarimbas@users.noreply.github.com>
2026-01-09 10:04:44 +0300

    ifs

--- main.gd
+++ main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-
+"""
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#print("Hello world!")
@@ -18,3 +18,20 @@ func _input(event: InputEvent) -> void:
 		
 	if event.is_action_released("jump"):
 		$Label.modulate = Color.AQUA
+
+"""
+
+var health = 100
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		health -= 20
+		print(health)
+		
+		if health <= 0:
+			health = 0
+			print("YOU DIED!")
+		elif health < 50:
+			print("Youre injured")
+		else:
+			print("Youre not dead yet")
