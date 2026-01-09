8877611ef7dfe9c66419d4f2340d581a1a80d3eb
qarimbas <71667807+qarimbas@users.noreply.github.com>
2026-01-09 09:57:16 +0300

    input

--- main.gd
+++ main.gd
@@ -3,9 +3,18 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Hello world!")
-
+	#print("Hello world!")
+	$Label.text = "Hello, world!"
+	$Label.modulate = Color.AQUA
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		$Label.modulate = Color.RED
+		
+	if event.is_action_released("jump"):
+		$Label.modulate = Color.AQUA
